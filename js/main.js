eefa41a7f61fbd20494111ae0d6a4f81e856e8b1
samuel <samuel.sukatja@gmail.com>
2025-01-24 17:26:47 +0700

    24-01-2025

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Initialize the tooltip
+var tooltipTriggerList = [].slice.call(
+	document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+	return new bootstrap.Tooltip(tooltipTriggerEl);
+});
